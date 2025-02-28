a9c2e18ef2bb259728f1a3e25d0513fda2eec16e
João Pedro <joaoofficialpedro@gmail.com>
2025-02-28 01:24:23 -0300

    Fix: exports in main package library

--- .link_interpreter/index.ts
+++ .link_interpreter/index.ts
@@ -1,10 +1,11 @@
 import { Interpreter } from 'interpreter';
+import { JsonTypes } from 'locales/json-structures.type';
 
-const interpreter = new Interpreter({
+const interpreter = new Interpreter<JsonTypes>({
     basePath: __dirname + '/locales'
 });
 
-console.log(interpreter.translate(''));
+console.log(interpreter.translate('HELLO'));
 
 interpreter.language = 'es';
-console.log(interpreter.translate(''));
+console.log(interpreter.translate('HELLO'));
---
+++ .link_interpreter/locales/json-structures.type.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Types to represent the JSON structures of the files
+ */
+export type JsonTypes = En | Es;
+
+export type JsonFilesType = "en" | "es";
+
+/**
+ * Structures of each JSON file represented in a specific type
+ * @see En
+ * @see Es
+ */
+
+export type En = {
+  HELLO: string;
+};
+export type Es = {
+  HELLO: string;
+};
