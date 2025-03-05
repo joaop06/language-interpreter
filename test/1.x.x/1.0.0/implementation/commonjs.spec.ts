9f193e0e6a5b20caf54c6c87be1b6684c5fb88c2
João Pedro <joaoofficialpedro@gmail.com>
2025-03-05 09:21:25 -0300

    Formatting

--- test/1.x.x/1.0.0/implementation/commonjs.spec.ts
+++ test/1.x.x/1.0.0/implementation/commonjs.spec.ts
@@ -1,272 +1,273 @@
-import { it, expect, describe } from 'vitest';
-import { generateEnv } from '../../../helper/helper';
+import { it, expect, describe } from "vitest";
+import { generateEnv } from "../../../helper/helper";
 
-describe.concurrent('CommonJs Implementation', () => {
+describe.concurrent("CommonJs Implementation", () => {
+  it("no such locales directory", () => {
+    const { exec, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
+    });
 
-    it('no such locales directory', () => {
-        const { exec, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true,
-        });
-
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    createFile(
+      `import { Interpreter } from 'interpreter';
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
                 basePath: "./locales"
-            });`
-        );
+            });`,
+    );
 
-        try {
-            exec(false);
-        } catch (e: any) {
-            const error = e.stderr.toString();
-            expect(error).toContain(`ENOENT: no such file or directory, scandir './locales'`);
-        } finally {
-            expect(() => exec()).toThrow();
-        }
+    try {
+      exec(false);
+    } catch (e: any) {
+      const error = e.stderr.toString();
+      expect(error).toContain(
+        `ENOENT: no such file or directory, scandir './locales'`,
+      );
+    } finally {
+      expect(() => exec()).toThrow();
+    }
+  });
+
+  it("should be able create a new instance of the Interpreter without locale files", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('should be able create a new instance of the Interpreter without locale files', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles();
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles();
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
-            });`
-        );
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
+            });`,
+    );
 
-        // Verifica a saída
-        expect(exec).not.toThrow();
+    // Verifica a saída
+    expect(exec).not.toThrow();
+  });
+
+  it("should return an error when trying to translate a key not found", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('should return an error when trying to translate a key not found', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true,
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles([
+      { example: { key: "value", key2: "value2" } },
+    ]);
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles([
-            { example: { key: 'value', key2: 'value2' } },
-        ]);
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
-            console.log(interpreter.translate('key1'));`
-        );
+            console.log(interpreter.translate('key1'));`,
+    );
 
-        try {
-            exec(false);
-        } catch (e: any) {
-            const error = e.stderr.toString();
-            expect(error).toContain('Exception: Key not found: key1');
-        } finally {
-            expect(exec).toThrow();
-        }
+    try {
+      exec(false);
+    } catch (e: any) {
+      const error = e.stderr.toString();
+      expect(error).toContain("Exception: Key not found: key1");
+    } finally {
+      expect(exec).toThrow();
+    }
+  });
+
+  it("should be able translate a example key", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('should be able translate a example key', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles([
+      { example: { key: "value", key2: "value2" } },
+    ]);
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles([
-            { example: { key: 'value', key2: 'value2' } },
-        ]);
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
-            console.log(interpreter.translate('key'));`
-        );
+            console.log(interpreter.translate('key'));`,
+    );
 
-        // Verifica a saída
-        expect(exec(false)).toBe('value');
-        expect(exec).not.toThrow();
+    // Verifica a saída
+    expect(exec(false)).toBe("value");
+    expect(exec).not.toThrow();
+  });
+
+  it("should be able set a new default language", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('should be able set a new default language', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles([
+      { example: { key: "value", key2: "value2" } },
+      { example2: { key3: "value3", key4: "value4" } },
+    ]);
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles([
-            { example: { key: 'value', key2: 'value2' } },
-            { example2: { key3: 'value3', key4: 'value4' } },
-        ]);
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
            
             interpreter.language = 'example2';
-            console.log(interpreter.translate('key3'));`
-        );
+            console.log(interpreter.translate('key3'));`,
+    );
 
-        // Verifica a saída
-        expect(exec(false)).toBe('value3');
-        expect(exec).not.toThrow();
+    // Verifica a saída
+    expect(exec(false)).toBe("value3");
+    expect(exec).not.toThrow();
+  });
+
+  it("should return an error when set a new language and trying translate a key not found", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('should return an error when set a new language and trying translate a key not found', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles([
+      { example: { key: "value", key2: "value2" } },
+      { example2: { key3: "value3", key4: "value4" } },
+    ]);
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles([
-            { example: { key: 'value', key2: 'value2' } },
-            { example2: { key3: 'value3', key4: 'value4' } },
-        ]);
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
             console.log(interpreter.translate('key'));
             
             interpreter.language = 'example2';
-            console.log(interpreter.translate('key5'));`
-        );
+            console.log(interpreter.translate('key5'));`,
+    );
 
-        try {
-            exec(false);
-        } catch (e: any) {
-            const error = e.stderr.toString();
-            expect(error).toContain('Exception: Key not found: key5');
-        } finally {
-            expect(exec).toThrow();
-        }
+    try {
+      exec(false);
+    } catch (e: any) {
+      const error = e.stderr.toString();
+      expect(error).toContain("Exception: Key not found: key5");
+    } finally {
+      expect(exec).toThrow();
+    }
+  });
+
+  it("should return an error when set a new language and trying translate a old key", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('should return an error when set a new language and trying translate a old key', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles([
+      { example: { key: "value", key2: "value2" } },
+      { example2: { key3: "value3", key4: "value4" } },
+    ]);
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles([
-            { example: { key: 'value', key2: 'value2' } },
-            { example2: { key3: 'value3', key4: 'value4' } },
-        ]);
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
             console.log(interpreter.translate('key'));
             
             interpreter.language = 'example2';
-            console.log(interpreter.translate('key'));`
-        );
+            console.log(interpreter.translate('key'));`,
+    );
 
-        try {
-            exec(false);
-        } catch (e: any) {
-            const error = e.stderr.toString();
-            expect(error).toContain('Exception: Key not found: key');
-        } finally {
-            expect(exec).toThrow();
-        }
+    try {
+      exec(false);
+    } catch (e: any) {
+      const error = e.stderr.toString();
+      expect(error).toContain("Exception: Key not found: key");
+    } finally {
+      expect(exec).toThrow();
+    }
+  });
+
+  it("it should be possible to implement the library and then type based on the generated JsonTypes file", () => {
+    const { exec, createLocaleFiles, createFile } = generateEnv({
+      module: "cjs",
+      returnString: true,
     });
 
-    it('it should be possible to implement the library and then type based on the generated JsonTypes file', () => {
-        const { exec, createLocaleFiles, createFile } = generateEnv({
-            module: 'cjs',
-            returnString: true
-        });
+    // Diretório temporário para os arquivos de traduções
+    const localesDir = createLocaleFiles([
+      { example: { key: "value", key2: "value2" } },
+    ]);
 
-        // Diretório temporário para os arquivos de traduções
-        const localesDir = createLocaleFiles([
-            { example: { key: 'value', key2: 'value2' } },
-        ]);
-
-        /**
-         * Cria o arquivo index no diretório temporário
-         * com o código para o teste da implementação
-         */
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    /**
+     * Cria o arquivo index no diretório temporário
+     * com o código para o teste da implementação
+     */
+    createFile(
+      `import { Interpreter } from 'interpreter';
 
             const interpreter = new Interpreter({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
-            console.log(interpreter.translate('key'));`
-        );
+            console.log(interpreter.translate('key'));`,
+    );
 
-        expect(() => exec(false)).not.toThrow();
+    expect(() => exec(false)).not.toThrow();
 
-        // Sobrescrever o arquivo index.ts com a nova importação
-        createFile(
-            `import { Interpreter } from 'interpreter';
+    // Sobrescrever o arquivo index.ts com a nova importação
+    createFile(
+      `import { Interpreter } from 'interpreter';
             import { JsonTypes } from './locales/json-structures.type';
 
             const interpreter = new Interpreter<JsonTypes>({
                 defaultLanguage: 'example',
-                basePath: "${localesDir.replace(/\\/g, '\\\\')}"
+                basePath: "${localesDir.replace(/\\/g, "\\\\")}"
             });
-            console.log(interpreter.translate('key'));`
-        );
+            console.log(interpreter.translate('key'));`,
+    );
 
-        expect(exec()).toBe('value');
-    });
+    expect(exec()).toBe("value");
+  });
 });
--- test/helper/helper.ts
+++ test/helper/helper.ts
@@ -1,114 +1,119 @@
-import { tmpdir } from 'os';
-import { join, resolve } from 'path';
-import { execSync } from 'child_process';
-import { mkdirSync, mkdtempSync, rmdirSync, writeFileSync } from 'fs';
+import { tmpdir } from "os";
+import { join, resolve } from "path";
+import { execSync } from "child_process";
+import { mkdirSync, mkdtempSync, rmdirSync, writeFileSync } from "fs";
 
-var folderNumber = 0;
-const versionsPath = resolve(join(__dirname, '../../versions'));
+let folderNumber = 0;
+const versionsPath = resolve(join(__dirname, "../../versions"));
 
 interface GenerateEnvInterface {
-    /** EnvironmentOptions */
-    libVersion?: string,
-    module: 'cjs' | 'esm',
+  /** EnvironmentOptions */
+  libVersion?: string;
+  module: "cjs" | "esm";
 
-    /** ExecuteOptions */
-    returnString?: boolean,
+  /** ExecuteOptions */
+  returnString?: boolean;
 }
 
 /**
  * Generate a environment with a temporary directory
  * to install and test the library version
- * 
- * @param module 
- * @param version 
- * @returns 
+ *
+ * @param module
+ * @param version
+ * @returns
  */
 export const generateEnv = ({
-    module,
-    returnString = false,
-    libVersion = 'interpreter-1.0.0.tgz',
+  module,
+  returnString = false,
+  libVersion = "interpreter-1.0.0.tgz",
 }: GenerateEnvInterface) => {
-    // Caminho do diretório temporário para o teste
-    folderNumber++;
-    const folderName = `interpreter-temp-folder-${folderNumber}`;
-    const tempDir = mkdtempSync(join(tmpdir(), folderName));
+  // Caminho do diretório temporário para o teste
+  folderNumber++;
+  const folderName = `interpreter-temp-folder-${folderNumber}`;
+  const tempDir = mkdtempSync(join(tmpdir(), folderName));
 
-    // Camihho para a versão da biblioteca que será instalada
-    const tarballPath = resolve(join(versionsPath, libVersion));
+  // Camihho para a versão da biblioteca que será instalada
+  const tarballPath = resolve(join(versionsPath, libVersion));
 
+  /**
+   * Instala a biblioteca no diretório temporário
+   * com base no módulo JavaScript utilizado
+   *
+   * Caso seja CommonJs (cjs), será criado o arquivo de configuração `tsconfig.json`
+   */
+  if (module === "esm") {
+    execSync(`npm init -y && npm install "${tarballPath}"`, { cwd: tempDir });
+  } else if (module === "cjs") {
+    execSync(`npm init -y && npm install "${tarballPath}" ts-node --save-dev`, {
+      cwd: tempDir,
+    });
 
-    /**
-     * Instala a biblioteca no diretório temporário
-     * com base no módulo JavaScript utilizado
-     * 
-     * Caso seja CommonJs (cjs), será criado o arquivo de configuração `tsconfig.json`
-     */
-    if (module === 'esm') {
-        execSync(`npm init -y && npm install "${tarballPath}"`, { cwd: tempDir });
+    const configFilePath = join(tempDir, "tsconfig.json");
+    writeFileSync(
+      configFilePath,
+      JSON.stringify({
+        compilerOptions: {
+          module: "commonjs",
+          target: "es2020",
+        },
+      }),
+    );
+  }
 
-    } else if (module === 'cjs') {
-        execSync(`npm init -y && npm install "${tarballPath}" ts-node --save-dev`, { cwd: tempDir });
-
-        const configFilePath = join(tempDir, 'tsconfig.json');
-        writeFileSync(
-            configFilePath,
-            JSON.stringify({
-                "compilerOptions": {
-                    "module": "commonjs",
-                    "target": "es2020"
-                }
-            })
-        );
-    }
-
-    return {
-        tempDir,
-        tarballPath,
-        createFile: createFile(tempDir),
-        createLocaleFiles: createLocaleFiles(tempDir),
-        exec: exec({ module, tempDir, returnString }),
-    };
+  return {
+    tempDir,
+    tarballPath,
+    createFile: createFile(tempDir),
+    createLocaleFiles: createLocaleFiles(tempDir),
+    exec: exec({ module, tempDir, returnString }),
+  };
 };
 
 const createLocaleFiles = (tempDir: string) => {
-    const localesDir = join(tempDir, 'locales');
+  const localesDir = join(tempDir, "locales");
 
-    return (files = []) => {
-        mkdirSync(localesDir, { recursive: true });
+  return (files = []) => {
+    mkdirSync(localesDir, { recursive: true });
 
-        for (const file of files) {
-            const fileName = Object.keys(file)[0];
-            const content = JSON.stringify(file[fileName]);
-            writeFileSync(join(localesDir, `${fileName}.json`), content);
-        }
-        return localesDir;
+    for (const file of files) {
+      const fileName = Object.keys(file)[0];
+      const content = JSON.stringify(file[fileName]);
+      writeFileSync(join(localesDir, `${fileName}.json`), content);
     }
-}
+    return localesDir;
+  };
+};
 
 const createFile = (tempDir: string) => {
-    return (content: string) => {
-        const testFilePath = join(tempDir, 'index.ts');
-        writeFileSync(testFilePath, content);
-        return testFilePath;
-    };
+  return (content: string) => {
+    const testFilePath = join(tempDir, "index.ts");
+    writeFileSync(testFilePath, content);
+    return testFilePath;
+  };
 };
 
 const exec = ({
-    module,
-    tempDir,
+  module,
+  tempDir,
 
-    /** ExecuteOptions */
-    returnString,
+  /** ExecuteOptions */
+  returnString,
 }: { tempDir: string } & Partial<GenerateEnvInterface>) => {
+  return (removeTempDir: boolean = true): string | Buffer<ArrayBufferLike> => {
+    const filePathToExecute = join(
+      tempDir,
+      module === "cjs" ? "index.ts" : "index.js",
+    );
 
-    return (removeTempDir: boolean = true): string | Buffer<ArrayBufferLike> => {
-        const filePathToExecute = join(tempDir, module === 'cjs' ? 'index.ts' : 'index.js');
+    const result = execSync(`npx ts-node "${filePathToExecute}"`, {
+      cwd: tempDir,
+      stdio: ["pipe", "pipe", "pipe"],
+    });
 
-        const result = execSync(`npx ts-node "${filePathToExecute}"`, { cwd: tempDir, stdio: ['pipe', 'pipe', 'pipe'] });
+    if (removeTempDir) rmdirSync(tempDir, { recursive: true });
 
-        if (removeTempDir) rmdirSync(tempDir, { recursive: true });
-
-        if (!!returnString) return result.toString().trim();
-        else return result;
-    }
+    if (!!returnString) return result.toString().trim();
+    else return result;
+  };
 };
